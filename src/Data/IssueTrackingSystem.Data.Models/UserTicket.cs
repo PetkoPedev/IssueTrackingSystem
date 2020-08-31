521920a76ceabc36d9503112a490decf91f783e9
Petko Pedev <petko.pedev@gmail.com>
2020-08-31 16:46:44 +0300

    Adding UserTicket table

---
+++ src/Data/IssueTrackingSystem.Data.Models/UserTicket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IssueTrackingSystem.Data.Models
+{
+    public class UserTicket
+    {
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public int TicketId { get; set; }
+
+        public Ticket Ticket { get; set; }
+    }
+}
