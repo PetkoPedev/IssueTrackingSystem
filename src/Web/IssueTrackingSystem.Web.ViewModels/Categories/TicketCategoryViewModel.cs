eeff9bf384f5bb9a680533a78342a6d14d6834e9
Petko Pedev <petko.pedev@gmail.com>
2020-11-27 17:37:34 +0200

    Adding a little fix to the layout, and adding a new functionality - categories controller and service.

---
+++ src/Web/IssueTrackingSystem.Web.ViewModels/Categories/TicketCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace IssueTrackingSystem.Web.ViewModels.Categories
+{
+    using IssueTrackingSystem.Data.Models;
+    using IssueTrackingSystem.Services.Mapping;
+
+    public class TicketCategoryViewModel : IMapFrom<Ticket>
+    {
+        public string Title { get; set; }
+
+        public string UserUserName { get; set; }
+
+        public int CommentsCount { get; set; }
+    }
+}
---
+++ src/Web/IssueTrackingSystem.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿namespace IssueTrackingSystem.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using IssueTrackingSystem.Services.Data.Category;
+    using IssueTrackingSystem.Web.ViewModels.Categories;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoriesService categoriesService;
+
+        public CategoriesController(ICategoriesService categoriesService)
+        {
+            this.categoriesService = categoriesService;
+        }
+
+        public IActionResult ByName(string name)
+        {
+            var viewModel = this.categoriesService.GetByName<CategoryViewModel>(name);
+            return this.View(viewModel);
+        }
+    }
+}
