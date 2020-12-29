3233059cacc49acb0ddb77696fd4cbcbfb230b18
Petko Pedev <petko.pedev@gmail.com>
2020-12-29 10:19:56 +0200

    Line added.

--- src/Web/IssueTrackingSystem.Web/Controllers/NotesController.cs
+++ src/Web/IssueTrackingSystem.Web/Controllers/NotesController.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+
     using IssueTrackingSystem.Data.Models;
     using IssueTrackingSystem.Services.Data.Notes;
     using IssueTrackingSystem.Web.ViewModels.Notes;
